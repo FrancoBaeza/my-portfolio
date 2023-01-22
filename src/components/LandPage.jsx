260bd7ea649dab0701a75b6c6ca61790fc88d892
FrancoBaeza <francobaezagraf@gmail.com>
2023-01-22 12:18:05 -0300

    implemented first icon

--- src/components/LandPage.jsx
+++ src/components/LandPage.jsx
@@ -1,17 +1,30 @@
+import GitHub from "./icons/GitHub";
+
 export default () => {
+  const iconsClasses =
+    "w-[25px] hover:-translate-y-1 duration-200 fill-base-primary cursor-pointer";
+
   return (
     <div className=" bg-cyan-900 h-screen">
-      <div className="px-[100px]">
+      {/* left fixed side */}
+      <div className="fixed left-0 bottom-0 z-100 w-[100px] flex flex-col items-center gap-4">
+        <div className="">
+          <GitHub className={iconsClasses} />
+        </div>
+        <span className="w-[1px] h-48 bg-base-secondary "></span>
+      </div>
+      <div className="px-[100px] h-[3000px]">
         {/* main central seccion */}
         <section className=" pt-[100px] w-full flex flex-col">
           <section className="flex flex-col gap-3 text-slate-200">
-            <p className=" font-medium text-lg font-mono tracking-tighter text-[#F1962E]">
+            <p className=" font-medium text-lg font-mono tracking-tighter text-base-secondary">
               Hi! I'm <span className="font-bold ">Franco</span>.
             </p>
-            <h1 className="text-[#C7F8FF] text-7xl tracking-tighter font-bold w-[70%] leading-[80px] font-markProBold">
-              I turn your ideas into reality.
+            <h1 className="text-base-primary text-7xl tracking-tighter font-bold w-[70%] leading-[80px] font-markProBold">
+              I turn your ideas into{" "}
+              <span className=" text-base-secondary">reality</span>.
             </h1>
-            <p className="text-[#C7F8FF] w-[600px] font-markPro">
+            <p className="text-base-primary w-[600px] font-markPro">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit,
               sed do eiusmod tempor incididunt ut labore et dolore
               magna aliqua. Ut enim ad minim veniam, quis nostrud
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        "base-secondary": "#F1962E",
+        "base-primary": "#C7F8FF",
+      },
       fontFamily: {
         mono: ["Mono", "monospace"],
         markPro: ["Regular", "sans-serif"],
