23f57b0e5c709de917e63fd72b8de5899d7e50ac
Franco Baeza Graf <francobaezagraf@gmail.com>
2023-01-31 17:40:00 -0300

    add: experience template

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,72 +1,180 @@
+import { useEffect } from "react";
 import { useState } from "react";
 
+import Icon from "./icons/SocialIcon";
+
 export default () => {
-    const [year , setYear] = useState(2020);
-    return (
-        <section className={`py-[100px] h-screen w-[1000px]`}>
-            {/* title */}
-            <div className="flex flex-row justify-center items-center gap-3">
-                {/* <hr className=" border-base-secondary border w-60"/> */}
-                <h2 className={`text-base-primary text-3xl tracking-tighter font-bold  leading-[80px] font-markProBold`} >My Road Map</h2>
-                <hr className=" border-base-secondary border w-96"/>
+  const [year, setYear] = useState(2023);
+  const [chg, setChg] = useState(true);
+
+  useEffect(() => {
+    setChg(false);
+    setTimeout(() => {
+      setChg(true);
+    }, 300);
+  }, [year]);
+
+
+  return (
+    <section className={`py-[100px] h-screen w-[1000px] flex flex-col gap-7`}>
+      {/* title */}
+      <div className="flex flex-row justify-center items-center gap-3">
+        {/* <hr className=" border-base-secondary border w-60"/> */}
+        <h2
+          className={`text-base-primary text-3xl tracking-tighter font-bold  leading-[80px] font-markProBold`}
+        >
+          My Road Map
+        </h2>
+        <hr className=" border-base-secondary border w-96" />
+      </div>
+
+      {/* body */}
+      <div className="flex flex-row px-[100px] gap-10">
+
+        {/* year buttons */}
+        <div className=" flex flex-col items-end">
+          <div className="flex flex-row items-center my-[-4px]">
+            <p
+              className={` ${
+                year === 2023 ? "text-base-primary" : "text-200-primary"
+              } duration-300 font-bold font-markProBold pr-1`}
+            >
+              2023
+            </p>
+            <button
+              onClick={() => setYear(2023)}
+              className={` ${
+                year === 2023 ? " bg-base-primary" : " bg-base-secondary"
+              } h-4 w-4 rounded-full duration-300`}
+            ></button>
+          </div>
+
+          <hr
+            className={` 
+              ${
+                year === 2023 &&
+                " bg-gradient-to-b from-base-primary via-base-secondary to-base-secondary"
+              } 
+              ${
+                year === 2022 &&
+                " bg-gradient-to-t from-base-primary via-base-secondary to-base-secondary"
+              } 
+              duration-300 border-0 bg-base-secondary w-[2px] h-20 mr-[7px]`}
+          />
+
+          <div className="flex flex-row items-center my-[-4px]">
+            <p
+              className={` ${
+                year === 2022 ? "text-base-primary" : "text-200-primary"
+              } duration-300 font-bold font-markProBold pr-1`}
+            >
+              2022
+            </p>
+            <button
+              onClick={() => setYear(2022)}
+              className={` ${
+                year === 2022 ? " bg-base-primary" : " bg-base-secondary"
+              } h-4 w-4 rounded-full duration-300`}
+            ></button>
+          </div>
+
+          <hr
+            className={` 
+              ${
+                year === 2021 &&
+                " bg-gradient-to-t from-base-primary via-base-secondary to-base-secondary"
+              } 
+              ${
+                year === 2022 &&
+                " bg-gradient-to-b from-base-primary via-base-secondary to-base-secondary"
+              } 
+              duration-300 border-0 bg-base-secondary w-[2px] h-20 mr-[7px]`}
+          />
+
+          <div className="flex flex-row items-center my-[-4px]">
+            <p
+              className={` ${
+                year === 2021 ? "text-base-primary" : "text-200-primary"
+              } duration-300 font-bold font-markProBold pr-1`}
+            >
+              2021
+            </p>
+            <button
+              onClick={() => setYear(2021)}
+              className={` ${
+                year === 2021 ? " bg-base-primary" : " bg-base-secondary"
+              } h-4 w-4 rounded-full duration-300`}
+            ></button>
+          </div>
+
+          <hr
+            className={` 
+              ${
+                year === 2021 &&
+                " bg-gradient-to-b from-base-primary via-base-secondary to-base-secondary"
+              } 
+              ${
+                year === 2020 &&
+                " bg-gradient-to-t from-base-primary via-base-secondary to-base-secondary"
+              } 
+              duration-300 border-0 bg-base-secondary w-[2px] h-20 mr-[7px]`}
+          />
+
+          <div className="flex flex-row items-center my-[-4px]">
+            <p
+              className={` ${
+                year === 2020 ? "text-base-primary" : "text-200-primary"
+              } duration-300 font-bold font-markProBold pr-1`}
+            >
+              2020
+            </p>
+            <button
+              onClick={() => setYear(2020)}
+              className={` ${
+                year === 2020 ? " bg-base-primary" : " bg-base-secondary"
+              } h-4 w-4 rounded-full duration-300`}
+            ></button>
+          </div>
+        </div>
+
+        {/* descriptions */}
+        <div className={`w-full h-full`}>
+          {year === 2023 && (
+            <div className={` ${chg ? "opacity-1" : "opacity-0"} duration-300 `}>
+              <h3 className=" text font-markPro font-semibold text-base-primary">
+                Today
+              </h3>
+              <div className="flex flex-col gap-3">
+                <span className="flex gap-3">
+                  <Icon
+                    icon="triangle"
+                    className={`h-2 w-2 fill-base-secondary rotate-90 mt-2 ml-2`}
+                  />
+                  <p className="text-400-primary w-[600px] font-markPro tracking-wide">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+                </span>
+
+                <span className="flex gap-3">
+                  <Icon
+                    icon="triangle"
+                    className={`h-2 w-2 fill-base-secondary rotate-90 mt-2 ml-2`}
+                  />
+                  <p className="text-400-primary w-[600px] font-markPro tracking-wide">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+                </span>
+              </div>
             </div>
-
-            {/* body */}
-            <div className="flex flex-row px-[100px]">
-                <div className=" flex flex-col items-end">
-                    <div className="flex flex-row items-center my-[-4px]">
-                        <p className={` ${ year === 2023 ? "text-base-primary" : "text-200-primary"} duration-300 font-bold font-markProBold pr-1`}>2023</p>
-                        <button 
-                            onClick={() => setYear(2023)} 
-                            className={` ${year === 2023 ? " bg-base-primary" : " bg-base-secondary"} h-4 w-4 rounded-full duration-300`}
-                        ></button>
-                    </div>
-
-                    <hr className={` 
-                        ${year === 2023 && " bg-gradient-to-b from-base-primary via-base-secondary to-base-secondary"} 
-                        ${year === 2022 && " bg-gradient-to-t from-base-primary via-base-secondary to-base-secondary"} 
-                        duration-300 border-0 bg-base-secondary w-[2px] h-20 mr-[7px]`}
-                    />
-
-                    <div className="flex flex-row items-center my-[-4px]">
-                        <p className={` ${ year === 2022 ? "text-base-primary" : "text-200-primary"} duration-300 font-bold font-markProBold pr-1`}>2022</p>
-                        <button 
-                            onClick={() => setYear(2022)} 
-                            className={` ${year === 2022 ? " bg-base-primary" : " bg-base-secondary"} h-4 w-4 rounded-full duration-300`}
-                        ></button>
-                    </div>
-
-                    <hr className={` 
-                        ${year === 2021 && " bg-gradient-to-t from-base-primary via-base-secondary to-base-secondary"} 
-                        ${year === 2022 && " bg-gradient-to-b from-base-primary via-base-secondary to-base-secondary"} 
-                        duration-300 border-0 bg-base-secondary w-[2px] h-20 mr-[7px]`}
-                    />
-
-                    <div className="flex flex-row items-center my-[-4px]">
-                        <p className={` ${ year === 2021 ? "text-base-primary" : "text-200-primary"} duration-300 font-bold font-markProBold pr-1`}>2021</p>
-                        <button 
-                            onClick={() => setYear(2021)} 
-                            className={` ${year === 2021 ? " bg-base-primary" : " bg-base-secondary"} h-4 w-4 rounded-full duration-300`}
-                        ></button>
-                    </div>
-
-                    <hr className={` 
-                        ${year === 2021 && " bg-gradient-to-b from-base-primary via-base-secondary to-base-secondary"} 
-                        ${year === 2020 && " bg-gradient-to-t from-base-primary via-base-secondary to-base-secondary"} 
-                        duration-300 border-0 bg-base-secondary w-[2px] h-20 mr-[7px]`}
-                    />
-
-                    <div className="flex flex-row items-center my-[-4px]">
-                        <p className={` ${ year === 2020 ? "text-base-primary" : "text-200-primary"} duration-300 font-bold font-markProBold pr-1`}>2020</p>
-                        <button 
-                            onClick={() => setYear(2020)} 
-                            className={` ${year === 2020 ? " bg-base-primary" : " bg-base-secondary"} h-4 w-4 rounded-full duration-300`}
-                        ></button>
-                    </div>
-                </div>
-
-
-            </div>
-        </section>
-    )
-}
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/components/icons/SocialIcon.jsx
+++ src/components/icons/SocialIcon.jsx
@@ -44,6 +44,21 @@ export default ({ className, url, icon }) => {
           <path d="M12 .02c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.99 6.98l-6.99 5.666-6.991-5.666h13.981zm.01 10h-14v-8.505l7 5.673 7-5.672v8.504z" />
         </svg>
       )}
+
+      {/* triangle */}
+      {icon === "triangle" && (
+        <svg 
+          clip-rule="evenodd" 
+          fill-rule="evenodd" 
+          stroke-linejoin="round" 
+          stroke-miterlimit="2" 
+          className={className}
+          viewBox="0 0 24 24" 
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="m2.095 19.882 9.248-16.5c.133-.237.384-.384.657-.384.272 0 .524.147.656.384l9.248 16.5c.064.115.096.241.096.367 0 .385-.309.749-.752.749h-18.496c-.44 0-.752-.36-.752-.749 0-.126.031-.252.095-.367z" fill-rule="nonzero"/>
+        </svg>
+      )}
     </a>
   );
 };
