4ea6ceefe123dd35c5c898228d00949a1b2f453c
franco baeza graf <francobaezagraf@gmail.com>
2025-07-17 20:25:44 -0300

    remv analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import Home from "./components/Home.jsx";
-import { Analytics } from "@vercel/analytics/next"
 
 export default function App() {
     return (
         <div className="App ">
             <Home />
-            <Analytics />
         </div>
     );
 }
