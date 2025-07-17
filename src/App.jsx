f339f56ae89636d748af59c978b3b69381d16be2
franco baeza graf <francobaezagraf@gmail.com>
2025-07-17 20:23:32 -0300

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Home from "./components/Home.jsx";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App() {
     return (
         <div className="App ">
             <Home />
+            <Analytics />
         </div>
     );
 }
